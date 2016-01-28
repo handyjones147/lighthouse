d822edbf89c79da69a0429bb4c71977f3c0a10ad
andela-ajayeoba <dejijaye@gmail.com>
2016-01-28 17:47:21 +0100

    add jquery

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $('nav a[href*=#]:not([href=#])').click(function() {
+    console.log("clicked");
+    // var current = $("nav").find(".nav-active");
+    // console.log(current);
+    // console.log(this);
+    // if($(this) !== current)
+    // {
+    //    current.removeClass("nav-active");
+    //    $(this).addClass("nav-active");
+    // }
+    
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      console.log(target);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 70
+        }, 2000);
+        return false;
+      }
+    }
+  });
+});
